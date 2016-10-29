089c75f49fc9c91d32911ceef064e6da7213142f
Krystian <krystian.skrzyszewski@gmail.com>
2016-10-29 08:28:25 +0200

    +1

--- src/CH03E22PointInCircleCheck.java
+++ src/CH03E22PointInCircleCheck.java
@@ -1,44 +1,48 @@
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-public class CH03E22 {
+public class CH03E22PointInCircleCheck {
 
 	public static void main(String[] args) {
 
 		Scanner scan = new Scanner(System.in);
-		double x = 0;
-		double y = 0;
+		double x = 0.0;
+		double y = 0.0;
+		boolean check = false;
 
-		while (x == 0) {
-			System.out.println("Enter a point with two coordinates: ");
-			x = scan.nextDouble();
-			if (!scan.hasNextDouble()) {
-				System.out.println("Wrong input");
-				x = 0;
+		while (true) {
+
+			while (check == false) {
+				System.out.println("Enter a point with two coordinates: ");
+				if (scan.hasNextDouble()) {
+					x = scan.nextDouble();
+					check = true;
+				} else {
+					System.out.println("Wrong input, try again");
+					scan.next();
+				}
 			}
-		}
 
-		while (y == 0) {
-			if (scan.hasNextDouble()) {
-				y = scan.nextDouble();
+			while (check == true) {
+				// scan.next();
+				if (scan.hasNextDouble()) {
+					y = scan.nextDouble();
+					check = false;
+				} else {
+					System.out.println("Wrong input, try again");
+					scan.next();
+				}
+			}
+
+			int r = 10;
+			double distance = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+
+			if (distance > r) {
+				System.out.printf("Point (%.1f, %.1f) is not in the circle %n", x, y);
 			} else {
-				System.out.println("Wrong input");
-				y = 0;
+				System.out.printf("Point (%.1f, %.1f) is in the circle %n", x, y);
 			}
-		}
 
-		/*
-		 * if(scan.hasNextDouble()){ y = scan.nextDouble(); } else{
-		 * System.out.println("Wrong input"); y = 0; continue; }
-		 */
-
-		int r = 10;
-		double distance = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
-
-		if (distance > r) {
-			System.out.printf("Point (%.1f, %.1f) is not in the circle %n", x, y);
-		} else {
-			System.out.printf("Point (%.1f, %.1f) is in the circle %n", x, y);
 		}
 
 	}

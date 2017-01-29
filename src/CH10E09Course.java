51d1bc26f3af465a2b9f65ab4819fbb7ae9d2cb5
Krystian <krystian.skrzyszewski@gmail.com>
2017-01-29 23:47:58 +0100

    New stuff from chapter 10

---
+++ src/CH10E09Course.java
@@ -0,0 +1,54 @@
+
+public class CH10E09Course {
+
+	private String courseName;
+	private String[] students = new String[3];
+	private int numberOfStudents;
+	
+	public void Course(String courseName){
+		this.courseName = courseName;
+	}
+	
+	public void addStudent(String student){
+		students[numberOfStudents] = student;
+		numberOfStudents++;
+		if(numberOfStudents==students.length){
+			String[] temp = new String[students.length + 1];
+			for (int i = 0; i < students.length; i++){
+				temp[i] = students[i];
+			}
+			students = temp;
+		}
+	}
+	
+	public String[] getStudents(){
+		return students;
+	}
+	
+	public int getNumberOfStudents(){
+		return numberOfStudents;
+	}
+	
+	public String getCourseName(){
+		return courseName;
+	}
+	
+	public void dropStudent(String student){
+		for(int i=0; i<students.length; i++){
+			if(student.equals(students[i])){
+				for(int j=i; j<students.length-1; j++){
+					students[j] = students[j+1];
+				}
+			}
+		}
+		numberOfStudents--;
+	}
+	
+	public void clear(){
+		for(int i=0; i<students.length; i++){
+			students[i] = null;
+		}
+	}
+	
+	
+}
---
+++ src/CH10E09TestCourse.java
@@ -0,0 +1,23 @@
+
+public class CH10E09TestCourse {
+
+	public static void main(String[] args) {
+		
+		CH10E09Course course1 = new CH10E09Course();
+		
+		course1.addStudent("1");
+		course1.addStudent("2");
+		course1.addStudent("3");
+		course1.addStudent("2");
+		course1.addStudent("3");
+		course1.addStudent("2");
+		
+		course1.dropStudent("3");
+		
+		String[] students = course1.getStudents();
+		for(int i=0; i<course1.getNumberOfStudents(); i++){
+			System.out.println(students[i] + " ");
+		}
+	}
+}
+
